c86d95d2eb29abcc96683a66116bb12ad47f87e1
Maumau <yujioshinozaki@gmail.com>
2022-01-03 18:49:55 +0800

    Added main level, environments, and cherries with all the animations.

--- Dodge Master/Assets/Scripts/CameraController.cs
+++ Dodge Master/Assets/Scripts/CameraController.cs
@@ -6,12 +6,6 @@ public class CameraController : MonoBehaviour
 {
     public Transform player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
